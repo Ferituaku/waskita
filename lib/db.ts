c910834d40382fc5ccefde07259fa91063c52ffc
Ferituaku <ferismegg123@gmail.com>
2025-10-10 22:47:24 +0700

    setup db

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 const dbConfig = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "1234",
   database: process.env.DB_NAME || "waskita_db",
 };
 
