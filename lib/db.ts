6577cd13131b5d83fd238dcc4ea12505f954e69c
faiqilmupadi <muhammadfaiqas88@gmail.com>
2025-10-18 06:49:32 +0700

    db baru

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ const dbConfig = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "waskita_db",
+database: process.env.DB_NAME || "railway", // ← Ubah dari "waskita_db" ke "railway"
 };
 
 const CREATE_DATABASE_SQL = `CREATE DATABASE IF NOT EXISTS \`${dbConfig.database}\`;`;
