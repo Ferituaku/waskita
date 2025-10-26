53fcba25808d1b908921bd5053368c946dcbf8e6
Ferituaku <ferismegg123@gmail.com>
2025-10-26 22:06:30 +0700

    UPDATE: berita baru

--- components/auth/slides.tsx
+++ components/auth/slides.tsx
@@ -37,6 +37,64 @@ export const slides: NewsSlide[] = [
     description:
       "Kolaborasi dengan berbagai komunitas dalam pembelajaran media intervensi.",
   },
+  {
+    src: "/news/workshop-wpa.jpg",
+    alt: "Foto Bersama Anggota WPA Kelurahan Tembalang",
+    title:
+      "Workshop Pembentukan Warga Peduli AIDS: Warga Tembalang Siap Jadi Agen Edukasi HIV AIDS",
+    category: "Workshop",
+    date: "2025-10-09",
+    author: "Choerun Nisya",
+    tags: ["wpa", "tembalang", "aids", "humaniora", "sosbud"],
+    source: "Stophiva Universitas Diponegoro",
+    description:
+      "Tim PPK Ormawa STOPHIVA Fakultas Kesehatan Masyarakat Universitas Diponegoro menggelar Workshop Pembentukan Warga Peduli AIDS (WPA) Kelurahan Tembalang.",
+  },
+  {
+    src: "/news/tim-stophiva-audiensi.png",
+    alt: "Audiensi Tim STOPHIVA dan Karang Taruna Tembalang",
+    title:
+      "Bangun Komitmen Bersama, Tim STOPHIVA Audiensi dengan Karang Taruna Tembalang Soal Edukasi HIV AIDS",
+    category: "Audiensi",
+    date: "2025-07-20",
+    author: "Surya Natanegara",
+    tags: ["AIDS", "Audiensi", "edukasi", "HIV", "StopHIVa"],
+    source: "LPM Publica Health",
+    description:
+      "Dalam memperluas jangkauan edukasi HIV AIDS di Kelurahan Tembalang, Tim PPK Ormawa STOPHIVA berkolaborasi dengan Karang Taruna Kelurahan Tembalang.",
+  },
+  {
+    src: "/news/gelar-transfer-learning.jpeg",
+    alt: "Anggota WPA Kelurahan Tembalang bersama Tim PPK Ormawa STOPHIVA",
+    title:
+      "Dari UNDIP untuk Warga: WPA Kelurahan Tembalang Gelar Transfer Learning Seputar HIV AIDS",
+    category: "Pengabdian Masyarakat",
+    date: "2025-09-30",
+    author: "Direktorat Jejaring Media, Komunitas, dan Komunikasi Publik",
+    tags: ["UNDIP", "WPA", "Tembalang", "HIV", "AIDS", "Transfer Learning"],
+    source: "Universitas Diponegoro",
+    description:
+      "Warga Peduli AIDS (WPA) Kelurahan Tembalang yang diinisiasi oleh Tim PPK Ormawa STOPHIVA dari Fakultas Kesehatan Masyarakat Universitas Diponegoro, sukses menggelar kegiatan transfer learning tentang HIV AIDS untuk warga Tembalang.",
+  },
+  {
+    src: "/news/Interview-ppk-stophiva.jpg",
+    alt: "Program Indepth Interview PPK STOPHIVA di Kelurahan Tembalang",
+    title:
+      "In-depth Interview PPK STOPHIVA di Tembalang Jadi Pondasi Pembentukan Warga Peduli AIDS",
+    category: "Wawancara",
+    date: "2025-10-09",
+    author: "Ghina Zalfaronaa",
+    tags: [
+      "PPK ORMAWA",
+      "Universitas Diponegoro",
+      "FKM UNDIP",
+      "STOPHIVA",
+      "Semarang",
+    ],
+    source: "NNC Netralnews",
+    description:
+      "Sebagai langkah awal dalam pembentukan Warga Peduli AIDS (WPA) di Kelurahan Tembalang, Tim PPK Ormawa STOPHIVA melakukan kegiatan in-depth interview kepada masyarakat.",
+  },
 ];
 
 export default slides;
